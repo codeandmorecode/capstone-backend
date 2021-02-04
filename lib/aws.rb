15b42b97033cf445de39880afd933a31f0e482c8
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2021-02-03 17:09:29 -0800

    added various to configure dynamodb locally

---
+++ lib/aws.rb
@@ -0,0 +1,58 @@
+module Aws
+
+  require 'time'
+
+  # Initialiaze access to DynamoDB
+  def self.init
+
+    # Set up Client with settings from secrets.yml:
+    if Rails.env.development?
+
+      @client ||= Aws::DynamoDB::Client.new(
+          endpoint: "http://localhost:8000",
+          region: "localhost"
+      )
+
+    else
+
+      @client ||= Aws::DynamoDB::Client.new(
+          access_key_id: Rails.application.secrets.dynamodb_access_key_id,
+          secret_access_key: Rails.application.secrets.dynamodb_secret_access_key,
+          region: Rails.application.secrets.region
+      )
+
+    end
+
+  end
+
+  # Save records in DynamoDB table
+  def self.save_items_to_my_table1(params)
+
+
+    return if !@client
+
+    resp = @client.put_item({
+                                item: {
+                                    "user_id" => params['user_id'].to_i,
+                                    "age" => params['age'],
+                                    "created_at" => Time.now.utc.iso8601,
+                                },
+                                return_consumed_capacity: "TOTAL",
+                                table_name: "my_table1",
+                            })
+
+  end
+
+  # Get all items from DynamoDB my_table1:
+  def self.get_items
+
+    resp = @client.scan({
+                            table_name: "my_table1",
+                        })
+
+    # Returns array of items:
+    return resp.items
+
+  end
+
+end
